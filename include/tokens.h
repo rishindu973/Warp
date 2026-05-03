c466b5de672e33e8d8ba532d3f41a6a360608f40
rishindu973 <IT24610795@my.sliit.lk>
2026-05-03 15:40:57 +0530

    Expand token enum for weekly tasks and failure states

--- include/tokens.h
+++ include/tokens.h
@@ -3,10 +3,36 @@
 
 typedef enum {
     TOKEN_EOF =0,
-    TOKEN_TASK, TOKEN_RUN, TOKEN_EVERY, TOKEN_DAY,
-    TOKEN_AT, TOKEN_AFTER, TOKEN_IF, TOKEN_SUCCESS,
-    TOKEN_IDENTIFIER, TOKEN_STRING, TOKEN_TIME,
-    TOKEN_LBRACE, TOKEN_RBRACE, TOKEN_ERROR
+    TOKEN_TASK,
+    TOKEN_RUN,
+    TOKEN_EVERY,
+    TOKEN_DAY,
+    TOKEN_WEEK,
+    TOKEN_ON,
+    TOKEN_SUNDAY,
+    TOKEN_MONDAY,
+    TOKEN_TUESDAY,
+    TOKEN_WEDNESDAY,
+    TOKEN_THURSDAY,
+    TOKEN_FRIDAY,
+    TOKEN_SATURDAY,
+    TOKEN_AT,
+    TOKEN_AFTER,
+    TOKEN_IF,
+    TOKEN_SUCCESS,
+    TOKEN_FAILURE,
+
+    /* Identifiers and Literals */
+    TOKEN_IDENTIFIER,  
+    TOKEN_STRING,      
+    TOKEN_TIME,
+
+    /* Structural Symbols */
+    TOKEN_LBRACE,     
+    TOKEN_RBRACE,
+    
+    /* Special */
+    TOKEN_ERROR
 } TokenType;
 
 #endif
